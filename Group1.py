7318772d1bfbd0cbec76163191ba291f6fedc3d0
its1sttimeee <jakkaf1rst@gmail.com>
2023-11-10 09:13:17 +0700

    Demo1

---
+++ Group1.py
@@ -0,0 +1,14 @@
+import turtle
+de = turtle.Turtle() 
+de.speed(1)
+de.shape("turtle")
+
+de.forward()
+de.
+de.
+de.
+de.
+de.
+
+
+turtle.done()
---
+++ test.py
@@ -0,0 +1,9 @@
+import turtle
+de = turtle.Turtle() # de is turtle name.
+de.speed(1)
+de.shape("turtle")
+
+de.forward(100)
+# de.left(90)
+
+turtle.done()
